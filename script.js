425ee0344313d1ecff338acdfa6e41c4aac5db4f
Mostafizur Rahman <59665707+imostafizur@users.noreply.github.com>
2023-07-13 12:34:44 +0600

    Password added

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
+var userInfo = null;
+
 document.getElementById("registrationForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var name = document.getElementById("nameInput").value;
     var id = document.getElementById("idInput").value;
     var mobile = document.getElementById("mobileInput").value;
     var email = document.getElementById("emailInput").value;
+    var password = document.getElementById("passwordInput").value;
     // Save the user in the database using SQL or an API call
     
-    // Simulate vaccination date assignment
-    var vaccinationDate = new Date().toDateString();
-    document.getElementById("vaccinationDate").textContent = "Your vaccination date is: " + vaccinationDate;
-    document.getElementById("vaccinationDateSection").style.display = "block";
+    // Simulate user data retrieval
+    userInfo = {
+        name: name,
+        id: id,
+        mobile: mobile,
+        email: email,
+        password: password
+    };
+    
+    document.getElementById("registrationForm").reset();
+    document.getElementById("registrationForm").style.display = "none";
+    document.getElementById("loginSection").style.display = "block";
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var loginId = document.getElementById("loginIdInput").value;
+    var loginPassword = document.getElementById("loginPasswordInput").value;
+    
+    // Authenticate the user using SQL or an API call
+    if (userInfo && userInfo.id === loginId && userInfo.password === loginPassword) {
+        document.getElementById("loginForm").reset();
+        document.getElementById("loginSection").style.display = "none";
+        document.getElementById("vaccinationDateSection").style.display = "block";
+        
+        // Simulate vaccination date assignment
+        var vaccinationDate = new Date().toDateString();
+        document.getElementById("vaccinationDate").textContent = "Your vaccination date is: " + vaccinationDate;
+    } else {
+        alert("Invalid login credentials. Please try again.");
+    }
 });
 
 document.getElementById("certificateButton").addEventListener("click", function() {
-    var name = document.getElementById("nameInput").value;
-    var id = document.getElementById("idInput").value;
-    var mobile = document.getElementById("mobileInput").value;
-    var email = document.getElementById("emailInput").value;
     // Generate certificate using SQL or an API call
     
-    var certificateText = "Certificate\n\nName: " + name + "\nNID/Passport Number: " + id + "\nMobile Number: " + mobile + "\nEmail: " + email + "\nVaccination Date: " + new Date().toDateString();
+    var certificateText = "Certificate\n\nName: " + userInfo.name + "\nNID/Passport Number: " + userInfo.id + "\nMobile Number: " + userInfo.mobile + "\nEmail: " + userInfo.email + "\nVaccination Date: " + new Date().toDateString();
     document.getElementById("certificateText").textContent = certificateText;
     document.getElementById("certificateSection").style.display = "block";
     
@@ -32,14 +58,11 @@ document.getElementById("certificateButton").addEventListener("click", function(
 
 // Simulate user account information
 document.addEventListener("DOMContentLoaded", function() {
-    var userInfo = {
-        name: "John Doe",
-        id: "1234567890",
-        mobile: "9876543210",
-        email: "johndoe@example.com"
-    };
-    var userInfoText = "Name: " + userInfo.name + "<br>NID/Passport Number: " + userInfo.id + "<br>Mobile Number: " + userInfo.mobile + "<br>Email: " + userInfo.email;
-    document.getElementById("userInfo").innerHTML = userInfoText;
+    if (userInfo !== null) {
+        var userInfoText = "Name: " + userInfo.name + "<br>NID/Passport Number: " + userInfo.id + "<br>Mobile Number: " + userInfo.mobile + "<br>Email: " + userInfo.email;
+        document.getElementById("userInfo").innerHTML = userInfoText;
+        document.getElementById("accountSection").style.display = "block";
+    }
 });
 
 // Function to generate PDF
